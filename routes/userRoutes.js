bb0eee83371d4dc1a6892d4f8b09dd07e270e0f0
aleksandregvrm <aleksandregvrm@gmail.com>
2024-02-22 12:49:04 +0400

    router changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,6 @@ router.route("/verify-email").get(verifyUser);
 router.route("/login").post(loginUser);
 router.route("/logout").delete(authenticateUser, logoutUser);
 router.route("/change-user").patch(authenticateUser,changeUser);
-// authentication needed here
 router.route("/change-password").post(changePassword);
 router.route("/reset-password").post(resetPassword);
 router.route("/delete-user").delete(authenticateUser,deleteUser);
